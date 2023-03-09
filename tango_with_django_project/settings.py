d891d8089cb4865f235a4ac4825f4096994f476e
daisyjam <daisy-jamieson@hotmail.co.uk>
2023-03-09 16:50:19 +0000

    fix secret exposed

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=#aph!85mr3tlj%7pzihm2t=6a8_iual)n(4ga$^@_$5ii!6ah'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
